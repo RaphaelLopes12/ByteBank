74a95a04dd38faf0a31a641dd8c2448799b991b0
Raphael Ferreira Lopes <raphaellopes228@gmail.com>
2022-04-30 17:06:58 -0300

    Created two methods, GET and SET

--- 06-ByteBank/ContaCorrente.cs
+++ 06-ByteBank/ContaCorrente.cs
@@ -4,49 +4,52 @@ namespace _06_ByteBank
 {
     public class ContaCorrente
     {
-        public Cliente titular;
+        public Cliente Titular  { get; set; }
+
         public int agencia;
         public int numero;
-        private double saldo = 100;
+        private double _saldo = 100;
 
-        public void DefinirSaldo(double saldo)
+        public double Saldo
         {
-            if(saldo < 0)
+            get
             {
-                return;
+                return _saldo;
             }
-                this.saldo = saldo;           
-        }
-
-        public double ObterSaldo()
-        {
-            return saldo;
+            set
+            {
+                if (value < 0)
+                {
+                    return;
+                }
+                _saldo = value;
+            }
         }
 
         public bool Sacar(double valor)
         {
-            if (saldo < valor)
+            if (_saldo < valor)
             {
                 return false;
             }
 
-            saldo -= valor;
+            _saldo -= valor;
             return true;
 
         }
 
         public void Depositar(double valor)
         {
-            saldo += valor;
+            _saldo += valor;
         }
 
         public bool Transferir(double valor, ContaCorrente contaDestino)
         {
-            if (this.saldo < valor)
+            if (this._saldo < valor)
             {
                 return false;
             }
-            this.saldo -= valor;
+            this._saldo -= valor;
             contaDestino.Depositar(valor);
             return true;
 
--- 06-ByteBank/Program.cs
+++ 06-ByteBank/Program.cs
@@ -12,10 +12,18 @@ namespace _06_ByteBank
         static void Main(string[] args)
         {
             ContaCorrente conta = new ContaCorrente();
+            Cliente cliente = new Cliente();
 
-            conta.DefinirSaldo(-10);
+            cliente.nome = "Raphael";
+            cliente.cpf = "705.784.281-30";
+            cliente.profissao = "Desenvolvedor";
 
-            Console.WriteLine(conta.ObterSaldo());
+            conta.Saldo = -10;
+            conta.Titular = cliente;
+
+            Console.WriteLine(conta.Titular.nome);
+            Console.WriteLine(conta.Saldo);
+            
 
             Console.ReadLine();
         }
